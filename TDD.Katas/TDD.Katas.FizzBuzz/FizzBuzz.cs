6f61435b88042d6482ac4f10322939c304bfef2f
btroncone <btroncone@gmail.com>
2014-06-10 05:54:46 -0400

    FizzBuzz Kata Day 4

--- TDD.Katas/TDD.Katas.FizzBuzz/FizzBuzz.cs
+++ TDD.Katas/TDD.Katas.FizzBuzz/FizzBuzz.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using NUnit.Framework;
 
@@ -6,93 +7,44 @@ namespace TDD.Katas.FizzBuzz
 {
     public class FizzBuzz
     {
-
-        public static string FizzBuzzPrinter()
+        public static string GetFizzBuzz()
         {
-            var fizzBuzzResult = PrintFizzBuzzNumbers();
+            var result = PrintFizzBuzzNumbers();
+            return result;
+        }       
+        public static string GetFizzBuzz(int numberToCheck)
+        {
+            IsNumberWithinRange(numberToCheck);
+            var fizzBuzzResult = string.Empty;
+            if (IsNumberFizz(numberToCheck)) fizzBuzzResult += "Fizz";
+            if (IsNumberBuzz(numberToCheck)) fizzBuzzResult += "Buzz";
+            if (string.IsNullOrEmpty(fizzBuzzResult)) fizzBuzzResult += numberToCheck;
             return fizzBuzzResult;
         }
-
-        public static string FizzBuzzPrinter(int numberToCheck)
-        {
-            IsNumberOutOfBounds(numberToCheck);
-            var fizzBuzzResult = GetAFizzBuzz(numberToCheck);
-            if (string.IsNullOrEmpty(fizzBuzzResult))
-            {
-                fizzBuzzResult = GetABuzz(numberToCheck);
-            }
-            if (string.IsNullOrEmpty(fizzBuzzResult))
-            {
-                fizzBuzzResult = GetAFizz(numberToCheck);
-            }
-            return fizzBuzzResult;
-        }
-
         private static string PrintFizzBuzzNumbers()
         {
-            var fizzBuzzResult = string.Empty;
+            var result = string.Empty;
             for (int i = 1; i <= 100; i++)
             {
-                var result = string.Empty;
-                if (IsNumberFizz(i)) result += "Fizz";
-                if (IsNumberBuzz(i)) result += "Buzz";
-                if (IsNormalNumber(result)) result += i;
-                fizzBuzzResult += " " + result;
+                result += GetFizzBuzz(i) + " ";
             }
-            return fizzBuzzResult.Trim();
+            return result.Trim();
+        }
+        private static bool IsNumberFizz(int numberToCheck)
+        {
+            return numberToCheck % 3 == 0;
+        }
+        private static bool IsNumberBuzz(int numberToCheck)
+        {
+            return numberToCheck % 5 == 0;
         }
 
-        private static bool IsNormalNumber(string currentResult)
+        private static void IsNumberWithinRange(int numberToCheck)
         {
-            return string.IsNullOrEmpty(currentResult);
-        }
-
-        private static void IsNumberOutOfBounds(int numberToCheck)
-        {
-            if (numberToCheck > 100 || numberToCheck < 0)
+            if (numberToCheck < 1 || numberToCheck > 100)
             {
                 throw new ArgumentOutOfRangeException();
             }
-        }
-
-        private static string GetAFizzBuzz(int numberToCheck)
-        {
-            var result = string.Empty;
-            if (IsNumberFizz(numberToCheck) && IsNumberBuzz(numberToCheck))
-            {
-                result += "FizzBuzz";
-            }
-            return result;
-        }
-
-        private static string GetAFizz(int numberToCheck)
-        {
-            var result = string.Empty;
-            if (IsNumberFizz(numberToCheck))
-            {
-                result += "Fizz";
-            }
-            return result;
-        }
-
-        private static string GetABuzz(int numberToCheck)
-        {
-            var result = string.Empty;
-            if (IsNumberBuzz(numberToCheck) && IsNumberBuzz(numberToCheck))
-            {
-                result += "Buzz";
-            }
-            return result;
-        }
-
-        private static bool IsNumberFizz(int numberToFizz)
-        {
-            return numberToFizz % 3 == 0;
-        }
-
-        private static bool IsNumberBuzz(int numberToBuzz)
-        {
-            return numberToBuzz % 5 == 0;
         }
     }
 }
--- TDD.Katas/TDD.Katas.FizzBuzz/UnitTests/FizzBuzzTests.cs
+++ TDD.Katas/TDD.Katas.FizzBuzz/UnitTests/FizzBuzzTests.cs
@@ -16,43 +16,48 @@ namespace TDD.Katas.FizzBuzz.UnitTests
         {
             _expected = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz"; 
         }
-
-        [TestCase(3, "Fizz")]
-        [TestCase(6, "Fizz")]
-        public void Will_Print_Fizz_For_Numbers_Divisible_By_3(int numberToCheck, string expectedResult)
+        
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        public void Will_Print_Fizz_If_Number_Is_Divisible_By_3(int numberToCheck)
         {
-            var actual = FizzBuzz.FizzBuzzPrinter(numberToCheck);
-            Assert.AreEqual(expectedResult, actual);
+            const string expected = "Fizz";
+            var actual = FizzBuzz.GetFizzBuzz(numberToCheck);
+            Assert.AreEqual(expected, actual);
         }
-        [TestCase(5, "Buzz")]
-        [TestCase(10, "Buzz")]
-        public void Will_Print_Buzz_For_Numbers_Divisible_By_5(int numberToCheck, string expectedResult)
+        [TestCase(5)]
+        [TestCase(20)]
+        [TestCase(100)]
+        public void Will_Print_Fizz_If_Number_Is_Divisible_By_5(int numberToCheck)
         {
-            var actual = FizzBuzz.FizzBuzzPrinter(numberToCheck);
-            Assert.AreEqual(expectedResult, actual);
+            const string expected = "Buzz";
+            var actual = FizzBuzz.GetFizzBuzz(numberToCheck);
+            Assert.AreEqual(expected, actual);
         }
-        [TestCase(15, "FizzBuzz")]
-        [TestCase(60, "FizzBuzz")]
-        public void Will_Print_FizzBuzz_For_Numbers_Divisible_By_3_And_5(int numberToCheck, string expectedResult)
+        [TestCase(15)]
+        [TestCase(30)]
+        [TestCase(45)]
+        public void Will_Print_FizzBuzz_If_Number_Is_Divisible_By_3_And_5(int numberToCheck)
         {
-            var actual = FizzBuzz.FizzBuzzPrinter(numberToCheck);
-            Assert.AreEqual(expectedResult, actual);
+            const string expected = "FizzBuzz";
+            var actual = FizzBuzz.GetFizzBuzz(numberToCheck);
+            Assert.AreEqual(expected, actual);
         }
         [Test]
-        public void Will_Print_1_Through_100_And_Make_Correct_Replacements()
+        public void Will_Print_Numbers_1_100_Inserting_Fizz_And_Buzz_Appropriately()
         {
-            var actual = FizzBuzz.FizzBuzzPrinter();
-            Assert.AreEqual(_expected, actual);
+            var fizzBuzzActualResult = FizzBuzz.GetFizzBuzz();
+            Assert.AreEqual(_expected, fizzBuzzActualResult);
         }
 
-        [TestCase(101)]
+        [ExpectedException(typeof (ArgumentOutOfRangeException))]
         [TestCase(0)]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void Will_Throw_Exception_When_Number_Out_Of_Bounds(int outOfBoundsNumber)
+        [TestCase(101)]
+        public void Will_Throw_Exception_If_Number_Is_Less_Than_1_Or_Greater_Than_100(int numberToCheck)
         {
-            FizzBuzz.FizzBuzzPrinter(101);
+            FizzBuzz.GetFizzBuzz(numberToCheck);
         }
-
         
     }
 }
